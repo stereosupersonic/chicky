cc1c1dcba2e2082fa5a2d4e85ad14aba64b07337
Michael Deimel <michael@deimel.de>
2012-06-29 17:17:52 +0200

    fixes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id               :integer          not null, primary key
+#  email            :string(255)      default(""), not null
+#  sign_in_count    :integer          default(0)
+#  name             :string(255)      not null
+#  crypted_password :string(255)      not null
+#  password_salt    :string(255)      not null
+#  login_count      :integer          default(0), not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
 require 'spec_helper'
 
 describe User do
